7ba909b96e85bd6abf6739025715853f96f8d6d9
Ahmed Soliman <cyborgsverige@gmail.com>
2021-06-24 13:34:24 +0200

    heroku crash fix

--- cmd/smsbot/main.go
+++ cmd/smsbot/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"os"
 	"smsbot/internal/Database"
 	"smsbot/internal/DiscordBot"
 )
 
 func main() {
+
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World!")
+	})
+
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil) //for heroku Port error
+
 	Database.Init()
 	DiscordBot.Init()
 }
