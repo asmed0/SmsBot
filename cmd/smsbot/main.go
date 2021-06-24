2fe3a87fc6475d4870864c6fdcb3b6dab2f29fc9
Ahmed Soliman <cyborgsverige@gmail.com>
2021-06-24 13:37:09 +0200

    bot init function not called fix

--- cmd/smsbot/main.go
+++ cmd/smsbot/main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
+	"SmsBot/internal/Database"
+	"SmsBot/internal/DiscordBot"
 	"fmt"
 	"net/http"
 	"os"
-	"smsbot/internal/Database"
-	"smsbot/internal/DiscordBot"
 )
 
 func main() {
+
+	Database.Init()
+	go DiscordBot.Init()
 
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello World!")
 	})
 
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil) //for heroku Port error
-
-	Database.Init()
-	DiscordBot.Init()
 }
