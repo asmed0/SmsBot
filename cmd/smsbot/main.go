d76ca2b9c5f57aa46091db1cdee6d057eda3964e
Ahmed Soliman <cyborgsverige@gmail.com>
2021-06-24 23:35:28 +0200

    fixed .env loading issue

--- cmd/smsbot/main.go
+++ cmd/smsbot/main.go
@@ -2,17 +2,13 @@ package main
 
 import (
 	"github.com/joho/godotenv"
-	"log"
 	"smsbot/internal/Database"
 	"smsbot/internal/DiscordBot"
 	"smsbot/internal/Topup"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	godotenv.Load()
 	Database.Init()
 	go DiscordBot.Init()
 
