07af9809ff39cb7ffcc5ac00f8f8e82aee0d53ae
Ahmed Soliman <cyborgsverige@gmail.com>
2021-06-26 14:53:13 +0200

    ignore messages outside commands channel

--- internal/DiscordBot/messageCreate.go
+++ internal/DiscordBot/messageCreate.go
@@ -3,6 +3,7 @@ package DiscordBot
 import (
 	"github.com/bwmarrin/discordgo"
 	"github.com/getsentry/raven-go"
+	"os"
 	"smsbot/configs"
 	"smsbot/internal/Database"
 	"smsbot/internal/SmsCodesIO"
@@ -20,6 +21,10 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// Ignore all messages created by the bot itself
 	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	//Ignore all messages outside #commands channel
+	if m.ChannelID != os.Getenv("commands_channel"){
 		return
 	}
 
