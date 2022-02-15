05c77e71daab9029897fe2cd5742e3170a95117c
Ahmed Soliman <gouda@MacBook-Pro.local>
2022-02-15 09:32:11 +0100

    virtual15

--- configs/FiveSimConfigs.go
+++ configs/FiveSimConfigs.go
@@ -9,7 +9,7 @@ func FiveSimConfigs(service string) *FiveSimStruct {
 	apikey := os.Getenv("5sim_apikey")
 	services := [][]string{
 		//servicename, opeator, country
-		{"other","virtual27", "russia"},
+		{"other","virtual15", "russia"},
 	}
 	//service slice handling
 	srvc := tools.SliceSlicer(services)
