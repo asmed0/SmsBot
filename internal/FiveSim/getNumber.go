ef08f0d57b96f02702cff6f39257d54aff0da0ec
Ahmed Soliman <gouda@MacBook-Pro.local>
2022-02-18 00:03:34 +0100

    made virtual27 default

--- internal/FiveSim/getNumber.go
+++ internal/FiveSim/getNumber.go
@@ -55,8 +55,8 @@ func getNumber(data *FiveSimSession) {
 		data.CreatedAt = jsonPtr.CreatedAt
 		data.Country = jsonPtr.Country
 	} else {
-		if data.Operator != "virtual27"{
-			data.Operator = "virtual27"
+		if data.Operator != "virtual15"{
+			data.Operator = "virtual15"
 			getNumber(data)
 		}else if data.Country != "india"{
 			data.Operator = "any"
